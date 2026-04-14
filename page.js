c80d7374c0cd3e9d24aafa5854c7ec452d634887
churchillkaron <patric@pcsphuket.com>
2026-04-15 04:44:56 +0700

    add root page to fix routing

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Churchill Control System</h1>
+
+      <a href="/control-final">Go to Control Panel</a>
+    </div>
+  );
+}

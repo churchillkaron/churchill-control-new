ba5bcbe1051a98ff152654e8f1ad54ee2a61742c
churchillkaron <patric@pcsphuket.com>
2026-04-18 00:20:16 +0700

    Fix layout: remove bg-black and apply global background image

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,47 @@
+"use client";
+
 import "./globals.css";
+import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body className="text-white">
+        <AppWrapper>{children}</AppWrapper>
+      </body>
+    </html>
+  );
+}
+
+function AppWrapper({ children }) {
+  const pathname = usePathname();
+  const isLanding = pathname === "/";
+
+  return (
+    <div className="relative min-h-screen">
+
+      {/* BACKGROUND IMAGE (ALL PAGES) */}
+      <div
+        className="fixed inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/bg-hero-control.jpg')" }}
+      />
+
+      {/* DARK OVERLAY ONLY FOR APP PAGES */}
+      {!isLanding && (
+        <div className="fixed inset-0 bg-black/50" />
+      )}
+
+      {/* CONTENT */}
+      <div className="relative z-10">
 
         <Navbar />
 
-        <main className="pt-20">
+        <main className="pt-24 px-6 max-w-7xl mx-auto">
           {children}
         </main>
 
-      </body>
-    </html>
+      </div>
+    </div>
   );
 }

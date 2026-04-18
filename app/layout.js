d35c4a36dd0253ea95763b59e4fef7cd310e36d2
churchillkaron <patric@pcsphuket.com>
2026-04-18 18:47:59 +0700

    Fix click issue: background overlay blocking UI interactions

--- app/layout.js
+++ app/layout.js
@@ -10,14 +10,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="text-white">
 
-        {/* 🌅 GLOBAL BACKGROUND */}
+        {/* BACKGROUND */}
         <div
-          className="fixed inset-0 z-0 bg-cover bg-center"
+          className="fixed inset-0 z-0 bg-cover bg-center pointer-events-none"
           style={{ backgroundImage: "url('/bg-beach.jpg')" }}
         />
 
         {/* DARK OVERLAY */}
-        <div className="fixed inset-0 z-0 bg-black/60" />
+        <div className="fixed inset-0 z-0 bg-black/60 pointer-events-none" />
 
         {/* CONTENT */}
         <div className="relative z-10">

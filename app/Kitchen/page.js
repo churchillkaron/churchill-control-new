15acdce17061f1449845c63bb67388b2e631a601
churchillkaron <patric@pcsphuket.com>
2026-04-18 13:08:20 +0700

    hard reset layout + kitchen route

---
+++ app/Kitchen/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import AppShell from "../AppShell";
+
+export default function Kitchen() {
+  return (
+    <AppShell>
+      <div className="text-3xl">
+        Kitchen Loaded
+      </div>
+    </AppShell>
+  );
+}

bc336b3919b4ef4d10df90306e553dbad3d0be1d
churchillkaron <patric@pcsphuket.com>
2026-04-18 13:43:42 +0700

    fix all AppShell imports globally

--- app/tables/page.js
+++ app/tables/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import AppShell from "./AppShell";
+import AppShell from "../AppShell";
 
 import { supabase } from "../../lib/supabase";
 

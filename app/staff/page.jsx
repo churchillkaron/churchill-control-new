82055eaac94de14b1ca60ccad72bddf7140da77b
churchillkaron <patric@pcsphuket.com>
2026-04-19 21:06:35 +0700

    Fix AppShell import path in staff page

--- app/staff/page.jsx
+++ app/staff/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import AppShell from "../../AppShell";
+import AppShell from "../AppShell";
 
 export default function StaffInvoices() {
   const [preview, setPreview] = useState(null);

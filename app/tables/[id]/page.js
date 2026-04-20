b29a8884345c28841448ace19189c858035bb3b8
churchillkaron <patric@pcsphuket.com>
2026-04-20 14:12:48 +0700

    Fix AppShell import path

--- app/tables/[id]/page.js
+++ app/tables/[id]/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import AppShell from "../AppShell";
+import AppShell from "../../AppShell";
 
 export default function TableDetailPage() {
   const { id } = useParams();

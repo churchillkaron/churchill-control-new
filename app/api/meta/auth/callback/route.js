14e316a0e6302199f104acf851a988cf3c8242d3
churchillkaron <patric@pcsphuket.com>
2026-05-09 20:44:30 +0700

    Support multi-page Meta save

--- app/api/meta/auth/callback/route.js
+++ app/api/meta/auth/callback/route.js
@@ -4,18 +4,30 @@ const BASE_URL =
   "https://app.churchillkaron.com";
 
 export async function GET(request) {
+
   const url = new URL(request.url);
 
-  const code = url.searchParams.get("code");
-  const state = url.searchParams.get("state");
+  const code =
+    url.searchParams.get("code");
+
+  const state =
+    url.searchParams.get("state");
 
   const savedState =
-    request.cookies.get("fb_oauth_state")?.value;
+    request.cookies.get(
+      "fb_oauth_state"
+    )?.value;
 
-  if (!code || !state || state !== savedState) {
+  if (
+    !code ||
+    !state ||
+    state !== savedState
+  ) {
+
     return NextResponse.redirect(
       `${BASE_URL}/marketing?facebook=error`
     );
+
   }
 
   const tokenUrl = new URL(
@@ -42,121 +54,150 @@ export async function GET(request) {
     code
   );
 
-  const tokenRes = await fetch(
-    tokenUrl.toString()
-  );
+  const tokenRes =
+    await fetch(
+      tokenUrl.toString()
+    );
 
   const tokenData =
     await tokenRes.json();
 
   console.log(
     "TOKEN DATA:",
-    JSON.stringify(tokenData, null, 2)
+    JSON.stringify(
+      tokenData,
+      null,
+      2
+    )
   );
 
   if (!tokenData.access_token) {
+
     return Response.json({
       success: false,
       tokenData,
-      message: "No access token",
+      message:
+        "No access token",
     });
+
   }
 
   // GET USER PAGES
 
-  const pagesRes = await fetch(
-    `https://graph.facebook.com/v23.0/me/accounts?access_token=${tokenData.access_token}`
-  );
+  const pagesRes =
+    await fetch(
+      `https://graph.facebook.com/v23.0/me/accounts?access_token=${tokenData.access_token}`
+    );
 
   const pagesData =
     await pagesRes.json();
 
   console.log(
     "PAGES DATA:",
-    JSON.stringify(pagesData, null, 2)
+    JSON.stringify(
+      pagesData,
+      null,
+      2
+    )
   );
 
   // SAFE GUARD
 
-if (!pagesData?.data?.length) {
+  if (
+    !pagesData?.data?.length
+  ) {
 
-  return Response.json({
-    success: false,
-    pagesData,
-    message: "No Facebook pages found",
-  });
+    return Response.json({
+      success: false,
+      pagesData,
+      message:
+        "No Facebook pages found",
+    });
 
-}
+  }
 
-// SAVE ALL PAGES
+  // SAVE ALL PAGES
 
-for (const page of pagesData.data) {
+  for (
+    const page of pagesData.data
+  ) {
 
-  const igRes = await fetch(
-    `https://graph.facebook.com/v23.0/${page.id}?fields=instagram_business_account&access_token=${page.access_token}`
-  );
+    const igRes =
+      await fetch(
+        `https://graph.facebook.com/v23.0/${page.id}?fields=instagram_business_account&access_token=${page.access_token}`
+      );
 
-  const igData =
-    await igRes.json();
+    const igData =
+      await igRes.json();
 
-  console.log(
-    "IG DATA:",
-    JSON.stringify(igData, null, 2)
-  );
+    console.log(
+      "IG DATA:",
+      JSON.stringify(
+        igData,
+        null,
+        2
+      )
+    );
 
-  const instagramId =
-    igData?.instagram_business_account?.id || null;
+    const instagramId =
+      igData
+        ?.instagram_business_account
+        ?.id || null;
 
-  await fetch(
-    `${BASE_URL}/api/meta/save-account`,
+    await fetch(
+      `${BASE_URL}/api/meta/save-account`,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type":
+            "application/json",
+        },
+
+        body: JSON.stringify({
+          connected: true,
+
+          access_token:
+            page.access_token,
+
+          page_name:
+            page.name,
+
+          page_id:
+            page.id,
+
+          instagram_business_id:
+            instagramId,
+        }),
+      }
+    );
+
+  }
+
+  const response =
+    NextResponse.redirect(
+      `${BASE_URL}/marketing?facebook=connected`
+    );
+
+  // USE FIRST PAGE TOKEN
+  // FOR SESSION COOKIE
+
+  response.cookies.set(
+    "fb_page_token",
+    pagesData.data[0]
+      .access_token,
     {
-      method: "POST",
-
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({
-
-        connected: true,
-
-        access_token:
-          page.access_token,
-
-        page_name:
-          page.name,
-
-        page_id:
-          page.id,
-
-        instagram_business_id:
-          instagramId,
-
-      }),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
     }
   );
 
-}
-
-const response =
-  NextResponse.redirect(
-    `${BASE_URL}/marketing?facebook=connected`
+  response.cookies.delete(
+    "fb_oauth_state"
   );
 
-response.cookies.set(
-  "fb_page_token",
-  firstPage.access_token,
-  {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  }
-);
+  return response;
 
-response.cookies.delete(
-  "fb_oauth_state"
-);
-
-return response;
 }

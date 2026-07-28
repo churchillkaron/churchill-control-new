43dd52a618a7e736fbee749c6f5dc3e6bebcec0f
churchillkaron <patric@pcsphuket.com>
2026-07-28 21:17:13 +0700

    add operations access control page

---
+++ app/(system)/workspace/[organizationId]/operations/access-control/page.jsx
@@ -0,0 +1,7 @@
+import OperationsAccessControlWorkCenter from "@/components/workspace/operations/OperationsAccessControlWorkCenter";
+
+export const dynamic = "force-dynamic";
+
+export default function OperationsAccessControlPage() {
+  return <OperationsAccessControlWorkCenter />;
+}

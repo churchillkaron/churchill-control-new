cbdab95ab1a9357652d1fe5d59ff55084460817a
churchillkaron <patric@pcsphuket.com>
2026-07-28 17:13:15 +0700

    wire canonical operations landing page

---
+++ app/(system)/workspace/[organizationId]/operations/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import OperationsWorkspaceHub from "@/components/workspace/operations/OperationsWorkspaceHub";
+
+export default function OperationsPage() {
+  return <OperationsWorkspaceHub />;
+}

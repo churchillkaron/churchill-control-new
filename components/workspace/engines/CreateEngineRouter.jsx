c296d56a29c1bfdf79d99d103c5beb60f73f98e7
churchillkaron <patric@pcsphuket.com>
2026-07-29 10:30:57 +0700

    Route Dimensions to dedicated Finance Dimension engine

--- components/workspace/engines/CreateEngineRouter.jsx
+++ components/workspace/engines/CreateEngineRouter.jsx
@@ -1,11 +1,16 @@
 "use client";
 
 import CreateEngine from "./CreateEngine";
+import FinanceDimensionEngine from "./FinanceDimensionEngine";
 import IntercompanyEngine, {
   isIntercompanyEngineAction,
 } from "./IntercompanyEngine";
 
 export default function CreateEngineRouter(props) {
+  if (String(props.moduleKey || "").toLowerCase() === "dimensions") {
+    return <FinanceDimensionEngine {...props} />;
+  }
+
   if (isIntercompanyEngineAction(props.action, props.moduleKey)) {
     return <IntercompanyEngine {...props} />;
   }

8b2c4227174578c2d2c6838d96f5bc3caa17a2fb
churchillkaron <patric@pcsphuket.com>
2026-07-31 10:04:04 +0700

    Run Creative direction approval before execution

--- scripts/run-creative-command.sh
+++ scripts/run-creative-command.sh
@@ -2,4 +2,5 @@
 set -eu
 
 node --loader ./scripts/next-alias-loader.mjs scripts/creative-command-preflight.mjs "$@"
+node --loader ./scripts/next-alias-loader.mjs scripts/creative-direction-approval.mjs "$@"
 exec node --loader ./scripts/next-alias-loader.mjs scripts/creative-command.mjs "$@"

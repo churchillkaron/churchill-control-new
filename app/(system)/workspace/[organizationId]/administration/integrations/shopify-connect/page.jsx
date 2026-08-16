db227514c1b2485569a776b268f1ebf7ced2a69d
churchillkaron <patric@pcsphuket.com>
2026-08-16 13:22:59 +0700

    Expose Shopify finance synchronization controls

--- app/(system)/workspace/[organizationId]/administration/integrations/shopify-connect/page.jsx
+++ app/(system)/workspace/[organizationId]/administration/integrations/shopify-connect/page.jsx
@@ -1,5 +1,6 @@
 import ShopifyIntegrationCard from "@/components/administration/integrations/ShopifyIntegrationCard";
 import ShopifyInventorySyncPanel from "@/components/administration/integrations/ShopifyInventorySyncPanel";
+import ShopifyFinanceSyncPanel from "@/components/administration/integrations/ShopifyFinanceSyncPanel";
 
 export default async function ShopifyConnectPage({ params }) {
   const resolved = await params;
@@ -9,6 +10,7 @@ export default async function ShopifyConnectPage({ params }) {
     <div className="min-h-screen bg-black">
       <ShopifyIntegrationCard organizationId={organizationId} />
       <ShopifyInventorySyncPanel organizationId={organizationId} />
+      <ShopifyFinanceSyncPanel organizationId={organizationId} />
     </div>
   );
 }

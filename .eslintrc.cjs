ba2453c5acf8f5b5fa29d27a8d1d5ed395245fea
churchillkaron <patric@pcsphuket.com>
2026-08-18 17:54:06 +0700

    Add deterministic Next 14 lint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "no-unused-vars": "off",
+    "@next/next/no-img-element": "warn",
+    "react/no-unescaped-entities": "warn"
+  }
+};

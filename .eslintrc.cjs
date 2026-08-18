510b5594309cbbd2e788b086a6d95e787cfe971b
churchillkaron <patric@pcsphuket.com>
2026-08-18 18:01:04 +0700

    Narrow lint exceptions to non-Next libraries and legacy hook workcenters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,23 @@ module.exports = {
   rules: {
     "no-unused-vars": "off",
     "@next/next/no-img-element": "warn",
-    "react/no-unescaped-entities": "warn"
-  }
+    "react/no-unescaped-entities": "warn",
+  },
+  overrides: [
+    {
+      files: ["lib/**/*.{js,jsx,ts,tsx}"],
+      rules: {
+        "@next/next/no-assign-module-variable": "off",
+      },
+    },
+    {
+      files: [
+        "components/workspace/engines/ExportEngine.jsx",
+        "components/workspace/master-data/MasterDataRuntimeWorkCenter.jsx",
+      ],
+      rules: {
+        "react-hooks/rules-of-hooks": "warn",
+      },
+    },
+  ],
 };

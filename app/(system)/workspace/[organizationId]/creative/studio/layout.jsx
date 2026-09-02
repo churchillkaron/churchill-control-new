cd62244af070845cfa97373475c062dfe82c9aa1
churchillkaron <patric@pcsphuket.com>
2026-09-02 12:38:31 +0700

    Add automatic Creative Studio layout

---
+++ app/(system)/workspace/[organizationId]/creative/studio/layout.jsx
@@ -0,0 +1,7 @@
+export default function CreativeStudioLayout({ children }) {
+  return (
+    <section className="h-[calc(100vh-112px)] min-h-0 overflow-hidden bg-[#050505]">
+      {children}
+    </section>
+  );
+}

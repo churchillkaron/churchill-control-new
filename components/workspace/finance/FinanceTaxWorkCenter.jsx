61c64eaffe379016ab72f43f09b090807f94ca4a
churchillkaron <patric@pcsphuket.com>
2026-09-04 19:04:13 +0700

    finance: wire amendment rail into tax cockpit

--- components/workspace/finance/FinanceTaxWorkCenter.jsx
+++ components/workspace/finance/FinanceTaxWorkCenter.jsx
@@ -2,11 +2,13 @@
 
 import FinanceTaxLegacyWorkCenter from "./FinanceTaxLegacyWorkCenter";
 import FinanceTaxCalendarRail from "./FinanceTaxCalendarRail";
+import FinanceTaxAmendmentRail from "./FinanceTaxAmendmentRail";
 
 export default function FinanceTaxWorkCenter(props) {
   return (
     <>
       <FinanceTaxCalendarRail organizationId={props.organizationId} entityId={props.entityId} />
+      <FinanceTaxAmendmentRail organizationId={props.organizationId} entityId={props.entityId} />
       <FinanceTaxLegacyWorkCenter {...props} />
     </>
   );

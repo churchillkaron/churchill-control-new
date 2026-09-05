e5315b211c41d51ea6d2e90b56217e8b2f4d1798
churchillkaron <patric@pcsphuket.com>
2026-09-05 07:52:55 +0700

    finance: attach durable coordination to Tax truth

--- components/workspace/finance/FinanceTaxWorkCenter.jsx
+++ components/workspace/finance/FinanceTaxWorkCenter.jsx
@@ -7,6 +7,7 @@ import FinanceTaxAmendmentRail from "./FinanceTaxAmendmentRail";
 import FinanceTaxSettlementRail from "./FinanceTaxSettlementRail";
 import FinanceTaxPortfolioRail from "./FinanceTaxPortfolioRail";
 import FinanceTaxCloseGuidanceRail from "./FinanceTaxCloseGuidanceRail";
+import FinanceTaxDependencyWorkRail from "./FinanceTaxDependencyWorkRail";
 
 export default function FinanceTaxWorkCenter(props) {
   // One filing selection is authoritative for every entity-level Tax control below.
@@ -17,6 +18,7 @@ export default function FinanceTaxWorkCenter(props) {
       <FinanceTaxPortfolioRail organizationId={props.organizationId} entityId={props.entityId} />
       <FinanceTaxCalendarRail organizationId={props.organizationId} entityId={props.entityId} selectedVatReturnId={selectedVatReturnId} />
       <FinanceTaxCloseGuidanceRail organizationId={props.organizationId} entityId={props.entityId} selectedVatReturnId={selectedVatReturnId} />
+      <FinanceTaxDependencyWorkRail organizationId={props.organizationId} entityId={props.entityId} selectedVatReturnId={selectedVatReturnId} />
       <FinanceTaxAmendmentRail organizationId={props.organizationId} entityId={props.entityId} selectedVatReturnId={selectedVatReturnId} />
       <FinanceTaxSettlementRail organizationId={props.organizationId} entityId={props.entityId} selectedVatReturnId={selectedVatReturnId} />
       <FinanceTaxLegacyWorkCenter

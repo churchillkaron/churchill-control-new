2decc9c8811d30ab4e150b1b0779c6cbb400e0de
churchillkaron <patric@pcsphuket.com>
2026-09-05 09:22:18 +0700

    Show Code intelligence in Code Studio

--- app/(system)/workspace/[organizationId]/creative/code/page.jsx
+++ app/(system)/workspace/[organizationId]/creative/code/page.jsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import CreativeCodeStudio from "@/components/creative/code/CreativeCodeStudio";
+import CodeEngineeringIntelligenceLiveCard from "@/components/operator/CodeEngineeringIntelligenceLiveCard";
 import CodeMissionHistoryPanel from "@/components/operator/CodeMissionHistoryPanel";
 
 export default async function CreativeCodePage({ params }) {
@@ -10,7 +11,11 @@ export default async function CreativeCodePage({ params }) {
   return (
     <div className="min-h-screen bg-[#080808]">
       <CreativeCodeStudio organizationId={organizationId} />
-      <div className="mx-auto max-w-[1500px] px-5 pb-8 md:px-8">
+      <div className="mx-auto max-w-[1500px] space-y-5 px-5 pb-8 md:px-8">
+        <CodeEngineeringIntelligenceLiveCard
+          organizationId={organizationId}
+          theme="dark"
+        />
         <CodeMissionHistoryPanel organizationId={organizationId} />
       </div>
     </div>

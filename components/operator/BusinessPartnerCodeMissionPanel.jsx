ff97d1cc421220113efcf2f1029c12d9f7dd655e
churchillkaron <patric@pcsphuket.com>
2026-09-05 09:22:11 +0700

    Show Code intelligence in Business Partner

--- components/operator/BusinessPartnerCodeMissionPanel.jsx
+++ components/operator/BusinessPartnerCodeMissionPanel.jsx
@@ -1,12 +1,19 @@
 "use client";
 
 import BusinessPartnerActiveCodeMissionPanel from "@/components/operator/BusinessPartnerActiveCodeMissionPanel";
+import CodeEngineeringIntelligenceLiveCard from "@/components/operator/CodeEngineeringIntelligenceLiveCard";
 import CodeMissionHistoryPanel from "@/components/operator/CodeMissionHistoryPanel";
 
 export default function BusinessPartnerCodeMissionPanel({ organizationId }) {
   return (
     <div data-avantiqo-business-partner-code-workspace="true">
       <BusinessPartnerActiveCodeMissionPanel organizationId={organizationId} />
+      <CodeEngineeringIntelligenceLiveCard
+        organizationId={organizationId}
+        theme="light"
+        compact
+        className="border-b border-black/[0.07] bg-[#FBFAF8] px-5 py-4"
+      />
       <CodeMissionHistoryPanel organizationId={organizationId} compact />
     </div>
   );

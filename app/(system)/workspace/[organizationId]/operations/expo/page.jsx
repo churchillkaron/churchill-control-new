20cd9b21bc52881ef4dd27d197e54be148f042b3
churchillkaron <patric@pcsphuket.com>
2026-09-06 11:56:27 +0700

    Mount restaurant expo pass workspace

---
+++ app/(system)/workspace/[organizationId]/operations/expo/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import RestaurantExpoPass from "@/components/workspace/operations/RestaurantExpoPass";
+
+export default function RestaurantExpoPage() {
+  return <RestaurantExpoPass />;
+}

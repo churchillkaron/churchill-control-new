f7c64c64905722dbb87b13778dde6efef2a3d8dd
churchillkaron <patric@pcsphuket.com>
2026-09-06 09:47:42 +0700

    Platform | Lock acquisition obligation trigger

---
+++ supabase/migrations/20260906095500_platform_acquisition_owner_obligation_trigger_lockdown.sql
@@ -0,0 +1,2 @@
+revoke all on function public.platform_cancel_acquisition_obligation_on_stage_change() from public, anon, authenticated;
+grant execute on function public.platform_cancel_acquisition_obligation_on_stage_change() to service_role;

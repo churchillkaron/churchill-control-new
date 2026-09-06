0f33531f76a246e58bde0173418f6ac6f2fbf89b
churchillkaron <patric@pcsphuket.com>
2026-09-06 13:08:37 +0700

    Theme restaurant kitchen with current Avantiqo design

---
+++ app/(system)/workspace/[organizationId]/operations/kitchen/layout.jsx
@@ -0,0 +1,5 @@
+import RestaurantAvantiqoTheme from "@/components/workspace/operations/RestaurantAvantiqoTheme";
+
+export default function RestaurantKitchenLayout({ children }) {
+  return <RestaurantAvantiqoTheme mode="production">{children}</RestaurantAvantiqoTheme>;
+}

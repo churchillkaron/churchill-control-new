9ed7a9c04a6f7b6d9c7a7f735ea184f9062ed0dd
churchillkaron <patric@pcsphuket.com>
2026-09-06 13:08:31 +0700

    Theme restaurant floor with current Avantiqo design

---
+++ app/(system)/workspace/[organizationId]/operations/tables/layout.jsx
@@ -0,0 +1,5 @@
+import RestaurantAvantiqoTheme from "@/components/workspace/operations/RestaurantAvantiqoTheme";
+
+export default function RestaurantTablesLayout({ children }) {
+  return <RestaurantAvantiqoTheme mode="service">{children}</RestaurantAvantiqoTheme>;
+}

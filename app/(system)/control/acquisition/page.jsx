6c38db128cefb1079bd786d2a360ce9f9e79b348
churchillkaron <patric@pcsphuket.com>
2026-09-06 11:48:24 +0700

    Platform | Mount atomic acquisition workbench

--- app/(system)/control/acquisition/page.jsx
+++ app/(system)/control/acquisition/page.jsx
@@ -1,4 +1,4 @@
-import PlatformAcquisitionControlPanel from "@/components/platform/PlatformAcquisitionControlPanel";
+import PlatformAcquisitionAtomicWorkbench from "@/components/platform/PlatformAcquisitionAtomicWorkbench";
 import PlatformAcquisitionOperationsPanel from "@/components/platform/PlatformAcquisitionOperationsPanel";
 
 export const dynamic = "force-dynamic";
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default function PlatformAcquisitionPage() {
   return (
     <main className="min-h-screen bg-[#F4F3EF] py-5">
-      <PlatformAcquisitionControlPanel />
+      <PlatformAcquisitionAtomicWorkbench />
       <PlatformAcquisitionOperationsPanel />
     </main>
   );

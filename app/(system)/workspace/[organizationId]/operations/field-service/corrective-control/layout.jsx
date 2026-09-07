924a5bb44a5a8ec6ea198fe889255b514292fe14
churchillkaron <patric@pcsphuket.com>
2026-09-07 08:18:34 +0700

    Guard Pest Control corrective control route

---
+++ app/(system)/workspace/[organizationId]/operations/field-service/corrective-control/layout.jsx
@@ -0,0 +1,5 @@
+import PestControlSolutionGate from "@/components/workspace/operations/pest-control/PestControlSolutionGate";
+
+export default function Layout({ children }) {
+  return <PestControlSolutionGate>{children}</PestControlSolutionGate>;
+}

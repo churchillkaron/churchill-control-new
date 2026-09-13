0f0a2caf36fa6f825f3f96381869e4a1e7da5d66
churchillkaron <patric@pcsphuket.com>
2026-09-13 18:35:31 +0700

    Simplify Business Partner and restore fast reads

--- components/operator/BusinessPartnerCodeMissionPanel.jsx
+++ components/operator/BusinessPartnerCodeMissionPanel.jsx
@@ -1,19 +1,5 @@
 "use client";
 
-import BusinessPartnerCodeActivitySummary from "@/components/operator/BusinessPartnerCodeActivitySummary";
-import CodeMissionStageRail from "@/components/operator/CodeMissionStageRail";
-import CodeProgressFeedProvider from "@/components/operator/CodeProgressFeedProvider";
-
-export default function BusinessPartnerCodeMissionPanel({ organizationId }) {
-  return (
-    <CodeProgressFeedProvider organizationId={organizationId}>
-      <div
-        data-avantiqo-business-partner-code-workspace="true"
-        data-avantiqo-code-progress-poll-owner="shared-provider"
-      >
-        <BusinessPartnerCodeActivitySummary organizationId={organizationId} />
-        <CodeMissionStageRail />
-      </div>
-    </CodeProgressFeedProvider>
-  );
+export default function BusinessPartnerCodeMissionPanel() {
+  return null;
 }
